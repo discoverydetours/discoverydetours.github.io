b7464181c2b34643494b4fd2bda8e18a0a1a4b19
dinosdigsdetours <dinosdigsdetours@gmail.com>
2024-06-27 20:51:08 -0700

    qr and appish

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+﻿const CACHE_NAME = 'dinos-digs-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html', // Add the rest of your page URLs
+  '/styles.css', // Add your CSS files
+  '/scripts.js', // Add your JS files
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+  // Add any other assets you want to cache
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
